24745653d04f681d3657052c502a661bd3b70444
Brandon Boatright <bboatright013@gmail.com>
2021-01-25 12:22:16 -0500

    updates for heroku

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.listen(process.env.PORT || 3001, () => {
-  if(process.env.PORT){
-    console.log(`Server starting on ${process.env.PORT}`)
-  } else {
-    console.log(`Server starting on port 3001`);
+
+    console.log(`Server starting`);
   }
 });
